3c14b43103b511d9f934e45409761e28ec3398ce
BenniaoManfei <987461420@qq.com>
2017-11-20 16:31:21 +0800

    新建网站微服务

---
+++ ls01-ms-website/README.md
@@ -0,0 +1,6 @@
+
+### ls01-ms-website ###
+
+课程1-微服务-网站
+
+1. 2017年11月20日14:53:58 Daniel·Dai Created
---
+++ ls01-ms-website/src/main/java/com/daniel/scloud/demo/ls01mswebsite/Ls01MsWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.scloud.demo.ls01mswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ls01MsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ls01MsWebsiteApplication.class, args);
+	}
+}
