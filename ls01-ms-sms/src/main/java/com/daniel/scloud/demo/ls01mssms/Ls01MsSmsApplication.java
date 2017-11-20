ab8607196233c5daed19a91cddb556673e4cfc07
BenniaoManfei <987461420@qq.com>
2017-11-20 13:46:34 +0800

    将短信微服务注册到注册中心

--- ls01-ms-sms/src/main/java/com/daniel/scloud/demo/ls01mssms/Ls01MsSmsApplication.java
+++ ls01-ms-sms/src/main/java/com/daniel/scloud/demo/ls01mssms/Ls01MsSmsApplication.java
@@ -2,8 +2,10 @@ package com.daniel.scloud.demo.ls01mssms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Ls01MsSmsApplication {
 
 	public static void main(String[] args) {
--- ls01-ms-sms/src/main/java/com/daniel/scloud/demo/ls01mssms/web/controller/UserController.java
+++ ls01-ms-sms/src/main/java/com/daniel/scloud/demo/ls01mssms/web/controller/UserController.java
@@ -5,6 +5,7 @@ import com.daniel.scloud.demo.ls01mssms.dao.UserRepository;
 import com.daniel.scloud.demo.ls01mssms.model.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,4 +38,10 @@ public class UserController {
         return users;
     }
 
+/*    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        User user = userRepository.findOne(id);
+
+        return user;
+    }*/
 }
