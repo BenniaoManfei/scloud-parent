c64e444cdfaf9541c568883fdef34aea48879280
BenniaoManfei <987461420@qq.com>
2017-12-13 23:09:02 +0800

    hystrix

--- ls05-ms-website-hystrix/src/main/java/com/daniel/scloud/demo/ls05mswebsitehystrix/Ls05MsWebsiteHystrixApplication.java
+++ ls05-ms-website-hystrix/src/main/java/com/daniel/scloud/demo/ls05mswebsitehystrix/Ls05MsWebsiteHystrixApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker//启动熔断机制
+@EnableHystrixDashboard //启动HystrixDashboard的监控
 public class Ls05MsWebsiteHystrixApplication {
 
 	public static void main(String[] args) {
